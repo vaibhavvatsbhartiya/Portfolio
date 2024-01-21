70607fa7ac02d324fc9f09507917081331a50b1c
Vaibhav vats <vabhavvats30@gmail.com>
2024-01-21 16:09:52 +0530

    add resume link

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Typed from 'react-typed';
+import Typed from "react-typed";
 import Button from "./Button";
-
 
 const Banner = () => {
   return (
@@ -12,25 +11,41 @@ const Banner = () => {
           <span className=" pl-2 font-bold text-blue-950 ">Vaibhav Vats.</span>
         </div>
         <div className=" p-1  mt-5 sm:mt-0">
-        I'm a 
-        <Typed 
-                strings={[
-                    'Front-End Developer.',
-                    'React  Developer.',
-                    'Wordpress Developer.']}
-                    typeSpeed={100}
-                    backSpeed={80}
-                    attr="placeholder"
-                    loop >
-                    <input className="bg-transparent  text-xl sm:text-3xl md:text-4xl pl-2" type="text"  />
-                </Typed>
+          I'm a
+          <Typed
+            strings={[
+              "Front-End Developer.",
+              "React  Developer.",
+              "Wordpress Developer.",
+            ]}
+            typeSpeed={100}
+            backSpeed={80}
+            attr="placeholder"
+            loop
+          >
+            <input
+              className="bg-transparent  text-xl sm:text-3xl md:text-4xl pl-2"
+              type="text"
+            />
+          </Typed>
         </div>
-        <div className=" text-sm  sm:text-lg p-2  md:m-4 mt-5 sm:mt-0 "> <span className="text-green-400 font-semibold sm:text-xl md:text-2xl text-lg  ">Current Status:</span>  Develop projects using React  and  Tailwind <br /> CSS. Also, Start learning about React Testing Library. </div>
-        <div className="mt-10" >
-        <Button text='Download CV'  />
+        <div className=" text-sm  sm:text-lg p-2  md:m-4 mt-5 sm:mt-0 ">
+          {" "}
+          <span className="text-green-400 font-semibold sm:text-xl md:text-2xl text-lg  ">
+            Current Status:
+          </span>{" "}
+          Develop projects using React and Tailwind <br /> CSS. Also, Start
+          learning about React Testing Library.{" "}
+        </div>
+        <div className="mt-10">
+          <a
+            href="https://docs.google.com/document/d/1seXJAIcPiqKxsK5k4MaYKsvkfrmMJ2y9jOJ63obcRik/edit?usp=sharing"
+            target="blank"
+          >
+            <Button text="Download CV" />
+          </a>
         </div>
       </div>
-      
     </div>
   );
 };
