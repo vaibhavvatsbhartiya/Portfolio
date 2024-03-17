215bd80b244487179c84f0f7bf1ae6ff01edf028
Vaibhav vats <76244950+vaibhavvatsbhartiya@users.noreply.github.com>
2024-03-17 18:02:14 +0530

    Fixes navbar click functionablit in desktop (#13)
    merge pr

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
@@ -8,18 +8,90 @@ import Service from "./Pages/Service";
 import UserContactForm from "./Pages/Contact";
 import Faq from "./Pages/Faq";
 
+// const App = () => {
+//   return (
+//     <div className="bg-gradient-to-r from-red-500 to-blue-500 ">
+//       <Header id="header" />
+//       <div id="banner" class="section">
+//         <Banner />
+//       </div>
+//       <div id="banner" class="section">
+//         <About />
+//       </div>
+//       <div id="banner" class="section">
+//         <Service />
+//       </div>
+//       <div id="banner" class="section">
+//         <Project />
+//       </div>
+//       <div id="banner" class="section">
+//         <Faq />
+//       </div>
+//       <div id="banner" class="section">
+//         <UserContactForm />
+//       </div>
+//     </div>
+//   );
+// };
+
 const App = () => {
+
+  const bannerRef = useRef(null);
+  const aboutRef = useRef(null);
+  const serviceRef = useRef(null);
+  const projectRef = useRef(null);
+  const faqRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const handleNavLinkClick = (id) => {
+    switch (id) {
+      case 'banner':
+        bannerRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'about':
+        aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'services':
+        serviceRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'project':
+        projectRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'faq':
+        faqRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'contact':
+        contactRef.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="bg-gradient-to-r from-red-500 to-blue-500 ">
-      <Header />
-      <Banner />
-      <About />
-      <Service />
-      <Project />
-      <Faq />
-      <UserContactForm />
+      <Header handleNavLinkClick={handleNavLinkClick} />
+      <div ref={bannerRef} className="section">
+        <Banner />
+      </div>
+      <div ref={aboutRef} className="section">
+        <About />
+      </div>
+      <div ref={serviceRef} className="section">
+        <Service />
+      </div>
+      <div ref={projectRef} className="section">
+        <Project />
+      </div>
+      <div ref={faqRef} className="section">
+        <Faq />
+      </div>
+      <div ref={contactRef} className="section">
+        <UserContactForm />
+      </div>
     </div>
   );
 };
+
 
 export default App;
